4259e257e54fd5501b0409a14acbd637da632b8f
ernmin <42770608+ernmin@users.noreply.github.com>
2025-01-03 23:48:15 +0800

    Change Structure to Nested Factory Functions

--- src/index.js
+++ src/index.js
@@ -1,52 +1,76 @@
+import { format, compareAsc } from "date-fns";
+
+function TodoController() {
+    const todolist = projects();
+    todolist.addProject(todolist.createProject('default'));
+    todolist.listProjects();
+
+    let counter = 0;
+    while(counter < 2) {
+        const projectName = prompt('Enter project name')
+        todolist.addProject(todolist.createProject(projectName));
+        counter++;
+    }
+    todolist.listProjects();
+    
+}
+
 function projects() {
-    projectarray = [];
-    const createProject = (name) => {
+    let projects = [];
+
+    function createProject(name) {
         const projectName = name;
         let tasks = [];
         const task = (title) => {
             let completed = false;
             let priority = 1;
+            const changePriority = (newPriority) => {
+                priority = newPriority;
+            }
             //add change priority method
             //add change completed method
             //add duedate and due date method
-            return { title, completed, priority };
+            return { title, completed, priority, changePriority, };
         } 
         const addTask = (description) => {
-        const newTask = task(description);
-        tasks.push(newTask);  
+            const newTask = task(description);
+            tasks.push(newTask);  
         };
         const deleteTask = (taskItemNum) => {
             tasks.splice(taskItemNum, 1);
         }
         
-        return { projectName, tasks, addTask, deleteTask, }
+        return { name, task, addTask, deleteTask, }
     }
 
-    const addProjectMethod = (projectObject) => {
-        projectarray.push(projectObject);
+    function addProject(projectObject) {
+        projects.push(projectObject);
     }
 
-    const deleteProjectMethod = (name) => {
+    function deleteProject(name) {
         const targetProjectIndex = projects.findIndex(obj => obj.projectName === name);
         if (targetProjectIndex > -1) {
-            projectarray.splice(targetProjectIndex, 1);
+            const removedProject = projects.splice(targetProjectIndex, 1);
+            console.log('Project ', removedProject, ' was removed');
+            return removedProject[0];
+        }
+        else{
+            console.log('Project not found');
         }
     }
 
-    let counter = 0;
-    addProjectMethod(createProject('default'));
-    while(counter < 2) {
-        const projectName = prompt('Enter project name')
-        addProjectMethod(createProject(projectName));
-        counter++;
-    }
-    
-    
+    function listProjects() {
+        console.log('Projects:', projects);
+        return projects;
+      }
 
-    return { projectarray, createProject, addProjectMethod, deleteProjectMethod };
+    
+    
+    return { createProject, addProject, deleteProject, listProjects, };
 }
 
-projects();
+
+
 
 
 function ScreenController() {
@@ -57,3 +81,5 @@ function ScreenController() {
 }
 
 //add local storage
+
+TodoController();
